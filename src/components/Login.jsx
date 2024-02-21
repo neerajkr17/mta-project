ad600e84b87d40805c7fee491b90fafc474a21cb
neerajkr17 <neeraj192837@gmail.com>
2024-02-21 16:05:11 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,14 +12,15 @@ const Login = () => {
     <div className='login-form'>
     <div className='container'>
         <div className='header'>
-            <div className='text'>Sign Up</div>
+            <div className='text'>{action}</div>
                 <div className='underline'></div>
         </div>
         <div className='inputs'>
-            <div className='input'>
+            {action==="Login" ? <div></div> :<div className='input'>
                 <img src={user_icon} alt=""/>
                 <input type='text' placeholder='Name'/>
-            </div>
+            </div>}
+            
 
             <div className='input'>
                 <img src={email_icon} alt=""/>
@@ -31,10 +32,11 @@ const Login = () => {
                 <input type='password' placeholder='Password'/>
             </div>
         </div>
-        <div className='forgot-password'>Lost Password ? <span>Click Here!</span></div>
+        {action==="Sign Up" ? <div></div> : <div className='forgot-password'>Forgot Password ? <span>Click Here!</span></div>}
+        
         <div className='submit-container'>
-            <div className='submit'>Sign Up</div>
-            <div className='submit'>Login</div>
+            <div className={action === "Login" ? "submit gray" : "submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
     </div>
     </div>
